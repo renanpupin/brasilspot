afc40067168c981ad1c0b590aba66d6939a73270
Renan Augusto Pupin de Oliveira <renan.pupin@gmail.com>
2015-10-14 23:15:11 -0300

    first layout integration

--- laravel/app/Servico.php
+++ laravel/app/Servico.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Servico extends Model
 {
-    //
+    protected $table = "Servico";
+
+    protected $fillable = array(
+        'Descricao');
 }
---
+++ laravel/public/assets/js/script.js
@@ -0,0 +1,47 @@
+// Ripple-effect animation
+(function($) {
+    "use strict";
+    $(".ripple").click(function(e){
+        var rippler = $(this);
+
+        // create .wave element if it doesn't exist
+        if(rippler.find(".wave").length == 0) {
+            rippler.append("<span class='wave'></span>");
+        }
+
+        var wave = rippler.find(".wave");
+
+        // prevent quick double clicks
+        wave.removeClass("animate");
+
+        // set .wave diametr
+        if(!wave.height() && !wave.width())
+        {
+            var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
+            wave.css({height: d, width: d});
+        }
+
+        // get click coordinates
+        var x = e.pageX - rippler.offset().left - wave.width()/2;
+        var y = e.pageY - rippler.offset().top - wave.height()/2;
+
+        // set .wave position and add class .animate
+        wave.css({
+            top: y+'px',
+            left:x+'px'
+        }).addClass("animate");
+    });
+
+})(jQuery, undefined);
+
+
+(function($) {
+    "use strict";
+
+    //script para verificar se o gotop aparece
+    //code here
+
+    //init dropdown
+    $("#sideMenu").dropdown();
+
+})(jQuery, undefined);
