709e26ee8e7d2a2cc5810c8cc67920f29f3ad9ae
Alisson Pereira <alissonerdx@outlook.com>
2015-10-25 13:11:07 -0200

    Rotas

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -96,28 +96,26 @@ Route::get('TrabalheConosco', function () {
 Route::get('Inicio', function () {
     return view('index');
 });
-<<<<<<< HEAD
+
 Route::get('/index', function () {
     return view('index');
 });
 
-// Authentication routes...
-Route::get('login', ['as' =>'login', 'uses' => 'Auth\AuthController@getLogin']);
-Route::post('login', ['as' =>'login', 'uses' => 'LoginController@logar']);
-//Route::post('login', ['as' =>'login', 'uses' => 'Auth\AuthController@postLogin']);
-Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\AuthController@getLogout']);
-// Registration routes...
-Route::get('register', ['as' =>'register', 'uses' => 'Auth\AuthController@getRegister']);
-Route::post('register', ['as' =>'register', 'uses' => 'Auth\AuthController@postRegister']);
-// Password reset link request routes...
-Route::get('forgot', ['as' =>'password/email', 'uses' => 'Auth\PasswordController@getEmail']);
-Route::post('forgot', ['as' =>'password/email', 'uses' => 'Auth\PasswordController@postEmail']);
-// Password reset routes...
-Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-Route::post('password/reset', 'Auth\PasswordController@postReset');
-=======
+//// Authentication routes...
+//Route::get('login', ['as' =>'login', 'uses' => 'Auth\AuthController@getLogin']);
+//Route::post('login', ['as' =>'login', 'uses' => 'LoginController@logar']);
+////Route::post('login', ['as' =>'login', 'uses' => 'Auth\AuthController@postLogin']);
+//Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\AuthController@getLogout']);
+//// Registration routes...
+//Route::get('register', ['as' =>'register', 'uses' => 'Auth\AuthController@getRegister']);
+//Route::post('register', ['as' =>'register', 'uses' => 'Auth\AuthController@postRegister']);
+//// Password reset link request routes...
+//Route::get('forgot', ['as' =>'password/email', 'uses' => 'Auth\PasswordController@getEmail']);
+//Route::post('forgot', ['as' =>'password/email', 'uses' => 'Auth\PasswordController@postEmail']);
+//// Password reset routes...
+//Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+//Route::post('password/reset', 'Auth\PasswordController@postReset');
 
 Route::get('/', function () {
     return redirect('Inicio');
 });
->>>>>>> origin/master
